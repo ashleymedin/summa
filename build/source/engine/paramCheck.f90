57ea2353499d7a8541e976c67b773871ad2f6627
ashleymedin <ashley.vanbeusekom@gmail.com>
2024-02-20 22:42:32 +0900

    Put in the 5 allocation errors found in the develop_sundials function, but still does not compile with gcc13

--- build/source/engine/paramCheck.f90
+++ build/source/engine/paramCheck.f90
@@ -115,9 +115,11 @@ contains
  ! canopy geometry
  heightCanopyTop        => mpar_data%var(iLookPARAM%heightCanopyTop)%dat(1),   & ! intent(in): [dp] height at the top of the vegetation canopy (m)
  heightCanopyBottom     => mpar_data%var(iLookPARAM%heightCanopyBottom)%dat(1),& ! intent(in): [dp] height at the bottom of the vegetation canopy (m)
- ! transpiration
- critSoilWilting        => mpar_data%var(iLookPARAM%critSoilWilting)%dat,      & ! intent(in): [dp] critical vol. liq. water content when plants are wilting (-)
- critSoilTranspire      => mpar_data%var(iLookPARAM%critSoilTranspire)%dat,    & ! intent(in): [dp] critical vol. liq. water content when transpiration is limited (-)
+ ! transpiration THIS IS A BUG IF NOT PUT IN AS (1)
+ !critSoilWilting        => mpar_data%var(iLookPARAM%critSoilWilting)%dat,      & ! intent(in): [dp] critical vol. liq. water content when plants are wilting (-)
+ !critSoilTranspire      => mpar_data%var(iLookPARAM%critSoilTranspire)%dat,    & ! intent(in): [dp] critical vol. liq. water content when transpiration is limited (-)
+ critSoilWilting        => mpar_data%var(iLookPARAM%critSoilWilting)%dat(1),   & ! intent(in): [dp] critical vol. liq. water content when plants are wilting (-)
+ critSoilTranspire      => mpar_data%var(iLookPARAM%critSoilTranspire)%dat(1), & ! intent(in): [dp] critical vol. liq. water content when transpiration is limited (-)
  ! soil properties
  fieldCapacity          => mpar_data%var(iLookPARAM%fieldCapacity)%dat(1),     & ! intent(in): [dp]    field capacity (-)
  theta_sat              => mpar_data%var(iLookPARAM%theta_sat)%dat,            & ! intent(in): [dp(:)] soil porosity (-)
@@ -161,7 +163,7 @@ contains
  end if
 
  ! check transpiration
- if( any(critSoilTranspire < critSoilWilting) )then
+ if( critSoilTranspire < critSoilWilting )then
   write(message,'(a,i0,a)') trim(message)//'critical point for transpiration is less than the wilting point'
   err=20; return
  endif
