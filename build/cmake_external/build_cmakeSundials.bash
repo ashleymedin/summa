fb81f6a51540b31974bfa16e668d68c4e7b80679
ashleymedin <ashley.vanbeusekom@gmail.com>
2024-04-23 16:30:17 +0900

    make files

--- build/cmake_external/build_cmakeSundials.bash
+++ build/cmake_external/build_cmakeSundials.bash
@@ -6,4 +6,4 @@
 # run `make`, then `make install`
 # Note, using -DEXAMPLES_ENABLE_C=OFF -DEXAMPLES_ENABLE_F2003=OFF, if want to run examples should change
 
-cmake ../../sundials-software/ -DEXAMPLES_ENABLE_C=OFF -DEXAMPLES_ENABLE_F2003=OFF -DBUILD_FORTRAN_MODULE_INTERFACE=ON -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_INSTALL_PREFIX=../../sundials/instdir -DEXAMPLES_INSTALL_PATH=../../sundials/instdir/examples
+cmake ../../sundials-software/ -DEXAMPLES_ENABLE_C=OFF -DEXAMPLES_ENABLE_F2003=OFF -DBUILD_FORTRAN_MODULE_INTERFACE=ON -DCMAKE_Fortran_COMPILER=gfortran -DENABLE_MAGMA=ON -DMAGMA_DIR=  -DENABLE_CUDA=ON -DCMAKE_INSTALL_PREFIX=../../sundials/instdir -DEXAMPLES_INSTALL_PATH=../../sundials/instdir/examples
