9c44b2fc2eb5f18465a23e5a3435bfe55dec52d6
ashleymedin <ashley.vanbeusekom@gmail.com>
2025-02-15 12:23:54 +0900

    utils

--- utils/post-processing/regroup_summa.py
+++ utils/post-processing/regroup_summa.py
@@ -13,7 +13,6 @@ import multiprocessing as mp
 
 # number of groups to divide the output into
 num_groups = 65
-chunk_size = 1000  # Number of records to process at a time
 gru_num = 517315  # Number of GRUs 
 
 missing = False  # if appending nan hrus to batch because failed
@@ -38,12 +37,14 @@ outfilelist0.sort()
 
 # -- functions
 def concatenate_files_in_range(outfilelist0, ctdir, start_gru, end_gru):
-    out_name = f'all_G{start_gru + 1:06d}-{end_gru:06d}.nc'
+    out_name = f'run1__G{start_gru + 1:06d}-{end_gru:06d}_timestep.nc'
     gru_num = 0
     hru_num = 0
 
     # Filter files within the specified range
-    filtered_files = [file for file in outfilelist0 if start_gru <= int(file.split('_')[1][1:7]) <= end_gru]
+    filtered_files = [file for file in outfilelist0 if start_gru <= int(file.split('_')[4][1:7]) <= end_gru]
+    start_gru_old = int(filtered_files[0].split('_')[4][1:7])
+    end_gru_old = int(filtered_files[-1].split('_')[4][8:14])
 
     for file in filtered_files:
         f = nc.Dataset(file)
@@ -53,7 +54,7 @@ def concatenate_files_in_range(outfilelist0, ctdir, start_gru, end_gru):
     # Write output
     with nc.Dataset(filtered_files[0]) as src:
         with nc.Dataset(ctdir + '/' + out_name, "w") as dst:
-            # Copy dimensions
+            # copy dimensions
             for name, dimension in src.dimensions.items():
                 if name == 'gru':
                     dst.createDimension(name, gru_num)
@@ -62,103 +63,94 @@ def concatenate_files_in_range(outfilelist0, ctdir, start_gru, end_gru):
                 else:
                     dst.createDimension(name, (len(dimension) if not dimension.isunlimited() else None))
 
-            # Copy variable attributes all at once via dictionary
-            gru_vars = []  # variable name, gru axis in variable dimension for concatenation.
+            # copy variable attributes all at once via dictionary
+            gru_vars = [] # variable name, gru axis in variable dimension for concatenation.
             hru_vars = []
             for name, variable in src.variables.items():
-                dst.createVariable(name, variable.datatype, variable.dimensions)
+                x = dst.createVariable(name, variable.datatype, variable.dimensions)
                 dst[name].setncatts(src[name].__dict__)
                 # Note here the variable dimension name is the same, but size has been updated for gru and hru.
 
                 # Assign different values depending on dimension
                 dims = variable.dimensions
                 if 'gru' in dims:
-                    gru_vars.append([name, dims.index('gru')])
+                    gru_vars.append([name,dims.index('gru')])
                 elif 'hru' in dims:
-                    hru_vars.append([name, dims.index('hru')])
+                    hru_vars.append([name,dims.index('hru')])
                 else:
-                    dst[name][:] = src[name][:]
+                    dst[name][:]=src[name][:]
 
+            # read values for gru and hru dimensioned variables
+            Dict = {}
             gru_vars_num = len(gru_vars)
             hru_vars_num = len(hru_vars)
-
-            # Read and write values for gru and hru dimensioned variables in chunks
             for i, file in enumerate(filtered_files):
-                print("combining file %d %s" % (i, file))
+
+                print("combining file %d %s" % (i,file))
+                # f = nc.Dataset(os.path.join(ncdir, file))
                 f = nc.Dataset(file)
                 for j in range(gru_vars_num):
                     gru_var_name = gru_vars[j][0]
-                    for start in range(0, len(f[gru_var_name]), chunk_size):
-                        end = min(start + chunk_size, len(f[gru_var_name]))
-                        dst.variables[gru_var_name][start:end] = f[gru_var_name][start:end]
+                    dim_index = gru_vars[j][1]
+                    data=f[gru_var_name][:]
+                    if i == 0:
+                        Dict[gru_var_name]=data
+                    else:
+                        Dict[gru_var_name]=np.concatenate((Dict[gru_var_name],data),axis=dim_index)
 
                 for j in range(hru_vars_num):
                     hru_var_name = hru_vars[j][0]
-                    for start in range(0, len(f[hru_var_name]), chunk_size):
-                        end = min(start + chunk_size, len(f[hru_var_name]))
-                        dst.variables[hru_var_name][start:end] = f[hru_var_name][start:end]
+                    dim_index = hru_vars[j][1]
+                    data=f[hru_var_name][:]
+                    if i == 0:
+                        Dict[hru_var_name]=data
+                    else:
+                        Dict[hru_var_name]=np.concatenate((Dict[hru_var_name],data),axis=dim_index)
 
-            # If missing HRUs, this is slow or broken
+            # assign values for gru and hru dimensioned variables
+            for j in range(gru_vars_num):
+                dst.variables[gru_vars[j][0]][:] = Dict[gru_vars[j][0]]
+            for j in range(hru_vars_num):
+                dst.variables[hru_vars[j][0]][:] = Dict[hru_vars[j][0]]
+
+            #if missing HRUs, this is slow or broken
             if missing:
-                new_index = np.append(dst["gru"][:], missgru)
+                new_index = np.append(dst["gru"].values,missgru)
                 dst.reindex({"gru": new_index})
                 dst.sel(gru=missgru)["gruId"] = missgru
 
-                new_index = np.append(dst["hru"][:], misshru)
+                new_index = np.append(dst["hru"].values,misshru)
                 dst.reindex({"hru": new_index})
                 dst.sel(gru=misshru)["hruId"] = misshru
+
+            # Remove grus and hrus that are not in the range
+            cut_start = start_gru - start_gru_old
+            cut_end = end_gru_old - end_gru
+            dst.variables['gru'][:] = dst.variables['gru'][cut_start:]
+            dst.variables['gru'][:] = dst.variables['gru'][:-cut_end]
+            dst.variables['hru'][:] = dst.variables['hru'][cut_start:]
+            dst.variables['hru'][:] = dst.variables['hru'][:-cut_end]
+
+            # Temporarily create gruId from hruId
+            #if gru_num == hru_num:
+            #    gruId = dst.createVariable('gruId', dst['hruId'].datatype, ('gru',))
+            #    gruId.long_name = "ID of group of response unit (GRU)"
+            #    gruId.units = dst['hruId'].units
+            #    dst.variables['gruId'][:] = dst.variables['hruId'][:]
+            #else:
+            #    print('Warning: gruId variable cannot be created since it has different size from hruId')
 
         print("wrote output: %s" % (ctdir + '/' + out_name))
 
     return out_name
 
-def divide_into_groups(all_file, ctdir, start_gru, end_gru):
-    with nc.Dataset(ctdir + '/' + all_file) as src:
-        hru_num = gru_num
-        start_hru = start_gru
-        end_hru = end_gru
-        out_name = f'run1__G{start_gru + 1:06d}-{end_gru:06d}_timestep.nc'
-
-        with nc.Dataset(ctdir + '/' + out_name, "w") as dst:
-            # Copy dimensions
-            for name, dimension in src.dimensions.items():
-                if name == 'gru':
-                    dst.createDimension(name, end_gru - start_gru)
-                elif name == 'hru':
-                    dst.createDimension(name, end_hru - start_hru)
-                else:
-                    dst.createDimension(name, (len(dimension) if not dimension.isunlimited() else None))
-
-            # Copy variable attributes all at once via dictionary
-            for name, variable in src.variables.items():
-                dst.createVariable(name, variable.datatype, variable.dimensions)
-                dst[name].setncatts(src[name].__dict__)
-
-                # Assign different values depending on dimension
-                dims = variable.dimensions
-                if 'gru' in dims:
-                    for start in range(start_gru, end_gru, chunk_size):
-                        end = min(start + chunk_size, end_gru)
-                        dst[name][start - start_gru:end - start_gru] = variable[start:end]
-                elif 'hru' in dims:
-                    for start in range(start_hru, end_hru, chunk_size):
-                        end = min(start + chunk_size, end_hru)
-                        dst[name][start - start_hru:end - start_hru] = variable[start:end]
-                else:
-                    dst[name][:] = variable[:]
-
-            print(f"wrote output: {ctdir + '/' + out_name}")
-            # remove the all file
-            os.remove(ctdir + '/' + all_file)
-
 # -- end functions
 
 def process_group(g):
     size_g = int(np.ceil(gru_num / num_groups))
-    start_gru = g * size_g
+    start_gru = g * size_g + 1
     end_gru = min((g + 1) * size_g, gru_num)
-    all_file = concatenate_files_in_range(outfilelist0, ctdir, start_gru, end_gru)
-    divide_into_groups(all_file, ctdir, start_gru, end_gru)
+    concatenate_files_in_range(outfilelist0, ctdir, start_gru, end_gru)
 
 # -- end functions
 
