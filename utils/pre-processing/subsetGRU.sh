80458d3af847517611038d6ae6433527f78d1f45
ashleymedin <ashley.vanbeusekom@gmail.com>
2025-07-09 16:35:31 +0900

    utils

--- utils/pre-processing/subsetGRU.sh
+++ utils/pre-processing/subsetGRU.sh
@@ -3,44 +3,41 @@
 #
 # Inside forcingFile_out need to change forcingPath to = desForcingPath
 # Inside forcingFile_out need to change initConditionFile, attributeFile, trialParamFile to = _${GRU_id} versions
-# written originally by A. Van Beusekom 2023, hardwired paths run on Copernicus
+# written originally by A. Van Beusekom 2025, hardwired paths run on Anvil
 
-module load StdEnv/2020
-module load gcc/9.3.0
-module load nco/5.0.6
+module load gcc/11.2.0
+module load nco/4.9.3
 
 # GRU want to subset, change to do another GRU
-#just set this as GRU_nc from G* on output error (here the equation output is 487981).
+#just set this as GRU_nc from output error of G* on output file (here the bad file is G069381)
 
-gruCount=518
+GRU_file=69381
 
-GRU_nc=487981
-
-GRU_id=$GRU_nc -1
+GRU_id=$((GRU_file-1))
 HRU_id=$GRU_id
-echo "HRU_nc and GRU nc are ${GRU_nc}, HRU and GRU id are ${HRU_id}"
+echo "GRU order in file is ${GRU_file} starting count at 1; id is ${GRU_id} in netcdf file"
 
 # top paths, change these to yours
-homeDir=/globalhome/gwu479/HPC/
-desforceDir=/project/gwf/gwf_cmt/gwu479/
-inpforceDir=/project/gwf/gwf_cmt/rez299/
-homePath=${homeDir}TestScripts/
+homeDir=$HOME/
+desforceDir=$PROJECT/users/ashley-vanbeusekom/basin_forcing/
+inpforceDir=$PROJECT/users/ashley-vanbeusekom/summaNorthAmerica_forcing/
+dessettingsPath=${homeDir}basin_settings/
+inpsettingsPath=${homeDir}summaNorthAmerica_settings/
 summa_exe=${homeDir}SummaSundials/summa/bin/summa_sundials.exe
-forcingPath=${inpforceDir}summaWorkflow_data/domain_NorthAmerica/forcing/4_SUMMA_input/
 
-# in paths, probably won't change
-fileManager_in=${homePath}fileManager.txt
-settingsPath=${homePath}settings/
-initConditionFile_in=${settingsPath}coldState.nc
-attributeFile_in=${settingsPath}attributes.nc
-trialParamFile_in=${settingsPath}trialParams.nc
+# in paths, probably won't change, but will need to make generic fileManager_XXXXXX.txt and put in ${dessettingsPath}
+# fileManager_XXXXXX.txt example provided in summa/utils/pre-processing
+fileManager_in=${dessettingsPath}fileManager_XXXXXX.txt
+initConditionFile_in=${inpsettingsPath}coldState.nc
+attributeFile_in=${inpsettingsPath}attributes.nc
+trialParamFile_in=${inpsettingsPath}trialParams.nc
 
 # out paths, probably won't change
-fileManager_out=${homePath}fileManager_${GRU_id}.txt
-initConditionFile_out=${settingsPath}coldState_${GRU_id}.nc
-attributeFile_out=${settingsPath}attributes_${GRU_id}.nc
-trialParamFile_out=${settingsPath}trialParams_${GRU_id}.nc
-desForcingPath=${desforceDir}summaWorkflow_data/domain_NorthAmerica/forcing/4_SUMMA_input_${GRU_id}/
+fileManager_out=${dessettingsPath}fileManager_${GRU_id}.txt
+initConditionFile_out=${dessettingsPath}coldState_${GRU_id}.nc
+attributeFile_out=${dessettingsPath}attributes_${GRU_id}.nc
+trialParamFile_out=${dessettingsPath}trialParams_${GRU_id}.nc
+desForcingPath=${desforceDir}basin_${GRU_id}/
 
 # set up directory and new file Manager (will have to change things in it manually as above)
 mkdir -p "$desForcingPath"
@@ -55,8 +52,8 @@ ncks -d hru,$HRU_id,$HRU_id $trialParamFile_in $trialParamFile_out
 echo "trialParams.nc HRU ${HRU_id} subsetted"
 
 # forcing subset has multiple files
-cd $forcingPath
-for fn in NorthAmerica_remapped_*00-00-00.nc; do
+cd $inpforceDir
+for fn in NorthAmerica_remapped_*00-00-00-chunked.nc; do
     output_fn=${desForcingPath}${fn}
     ncks -d hru,$HRU_id,$HRU_id $fn $output_fn
     echo "${fn} HRU ${HRU_id} subsetted"
@@ -64,5 +61,5 @@ done
 cd $homePath
 
 # write summa command call file
-runFile=${homePath}run_${GRU_id}.sh
+runFile=${dessettingsPath}run_${GRU_id}.sh
 echo "${summa_exe} -p never -s _testSumma -m ${fileManager_out} -r e" > $runFile
