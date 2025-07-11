ab2b70bb18ab0a0c7f1cd63fdbb9a65dd90fbf3b
ashleymedin <ashley.vanbeusekom@gmail.com>
2025-07-12 02:17:45 +0900

    more of splitting

--- build/source/dshare/flxMapping.f90
+++ build/source/dshare/flxMapping.f90
@@ -177,7 +177,7 @@ contains
  ! derived variables
  flux2state_orig(iLookFLUX%scalarTotalET)                   = flux2state(state1=iname_nrgCanopy, state2=iname_nrgLayer)
  flux2state_orig(iLookFLUX%scalarTotalRunoff)               = flux2state(state1=iname_matLayer,  state2=integerMissing)
- flux2state_orig(iLookFLUX%scalarGlacierMelt)               = flux2state(state1=iname_watLayer,  state2=iname_watGlce)
+ flux2state_orig(iLookFLUX%scalarGlacierMelt)               = flux2state(state1=iname_watLayer,  state2=iname_matLayer)
  flux2state_orig(iLookFLUX%scalarNetRadiation)              = flux2state(state1=iname_nrgCanopy, state2=iname_nrgLayer)
 
  ! ** copy across flux metadata
