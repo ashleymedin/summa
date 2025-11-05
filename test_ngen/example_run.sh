0920e1106d82380855b8d324d3b7e6c5ba661e25
ashleymedin <ashley.vanbeusekom@gmail.com>
2025-11-05 19:49:34 +0900

    Merge branch 'master' of https://github.com/ashleymedin/summa
    * 'master' of https://github.com/ashleymedin/summa: (37 commits)
      spaces
      more spaces
      put some spaces back in that were there
      makefile was filled with weird values
      fixing allocation errors and uninitialized values
      updated what's new with PR #540 change
      with change in scalarthroughfallRain to matLayer state, need to make sure effective rainfall is only computed with snow layers
      Prepared develop for v3.2.0 release PR
      Revert "Merge pull request #525 from seantrim/develop_refactor"
      Revert "Revert cosmetic prs"
      Revert "Merge pull request #525 from seantrim/develop_refactor"
      Revert "updated whats-new file for v3.2.0 release"
      updated whats-new file for v3.2.0 release
      include snow drainage in model output
      check that macropore hydraulic conductivity is greater than micropore
      Update to whats-new.md
      Updates to soilLiqFlx.f90 and vegNrgFlux.f90 -- expanded some comments, improved readability, retained legacy code comments, and removed unused printflag logical.
      Added comments to compile_SUMMA.sh script to clarify usage and prerequisites.
      Minor formatting updates to vegNrgFlux.f90
      Described flux refactoring changes in whats-new.md and corrected minor issue in gitignore file.
      ...
    
    # Conflicts:
    #	build/makefiles/Sundials/makefile_cluster
    #	build/source/dshare/globalData.f90
    #	build/source/dshare/popMetadat.f90
    #	build/source/engine/computFlux.f90
    #	build/source/engine/computResid.f90
    #	build/source/engine/coupled_em.f90
    #	build/source/engine/eval8summa.f90
    #	build/source/engine/groundwatr.f90
    #	build/source/engine/opSplittin.f90
    #	build/source/engine/soilLiqFlx.f90
    #	build/source/engine/vegNrgFlux.f90
    #	case_study/base_settings/readme.md

--- test_ngen/example_run.sh
+++ test_ngen/example_run.sh
@@ -1,4 +1,5 @@
 #./cmake_build/ngen ./data/gauge_01073000/gauge_01073000.gpkg '' ./test/data/routing/gauge_01073000.gpkg '' ./extern/summa/summa/test_ngen/example_realization_config_w_summa_bmi_routing.json
+#rm test/data/routing/*.parquet
 
 ./cmake_build/ngen ./data/gauge_01073000/gauge_01073000.gpkg '' ./test/data/routing/gauge_01073000.gpkg '' ./extern/summa/summa/test_ngen/example_realization_config_w_summa_bmi.json
 
