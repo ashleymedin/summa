fcb83ccefa97cfaf417f3c42812da269541fd17e
ashleymedin <ashley.vanbeusekom@gmail.com>
2025-11-05 21:11:53 +0900

    bmi stuff

--- test_ngen/example_run.sh
+++ test_ngen/example_run.sh
@@ -2,5 +2,3 @@
 #rm test/data/routing/*.parquet
 
 ./cmake_build/ngen ./data/gauge_01073000/gauge_01073000.gpkg '' ./test/data/routing/gauge_01073000.gpkg '' ./extern/summa/summa/test_ngen/example_realization_config_w_summa_bmi.json
-
-#./cmake_build/ngen ./data/gauge_01073000/gauge_01073000.gpkg '' ./test/data/routing/gauge_01073000.gpkg '' ./data/gauge_01073000/example_bmi_multi_realization_config_w_routing.json
