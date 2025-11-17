f0b19b8f27683410fd8639c5f9b4a3295521b29e
ashleymedin <ashley.vanbeusekom@gmail.com>
2025-11-18 08:43:00 +0900

    gitignore

--- test_ngen/provo_run.sh
+++ test_ngen/provo_run.sh
@@ -1,4 +1,6 @@
-#./cmake_build/ngen ./test/data/routing/gauge_01073000.gpkg '' ./test/data/routing/gauge_01073000.gpkg '' ./extern/summa/summa/test_ngen/provo_realization_config_w_summa_bmi_routing.json
+#./cmake_build/ngen ./extern/summa/summa/test_ngen/domain_provo/settings/gage-10154200_subset.gpkg '' ./extern/summa/summa/test_ngen/domain_provo/settings/gage-10154200_subset.gpkg'' ./extern/summa/summa/test_ngen/settings/provo_realization_config_w_summa_bmi_routing.json
 #rm extern/summa/summa/test_ngen/domain_provo/simulations/*.parquet
 
-./cmake_build/ngen ./test/data/routing/gauge_01073000.gpkg '' ./test/data/routing/gauge_01073000.gpkg '' ./extern/summa/summa/test_ngen/provo_realization_config_w_summa_bmi.json
+./cmake_build/ngen ./extern/summa/summa/test_ngen/domain_provo/settings/gage-10154200_subset.gpkg '' ./extern/summa/summa/test_ngen/domain_provo/settings/gage-10154200_subset.gpkg '' ./extern/summa/summa/test_ngen/domain_provo/settings/provo_realization_config_w_summa_bmi.json
+
+#rm extern/summa/summa/test_ngen/domain_provo/simulations/*.parquet
