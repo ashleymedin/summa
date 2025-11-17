e2bcb86fc4dc24558d0d67d72de8f73c818654a3
ashleymedin <ashley.vanbeusekom@gmail.com>
2025-11-18 08:47:58 +0900

    sorting test basin

---
+++ test_ngen/provo_run.sh
@@ -0,0 +1,4 @@
+#./cmake_build/ngen ./test/data/routing/gauge_01073000.gpkg '' ./test/data/routing/gauge_01073000.gpkg '' ./extern/summa/summa/test_ngen/provo_realization_config_w_summa_bmi_routing.json
+#rm extern/summa/summa/test_ngen/domain_provo/simulations/*.parquet
+
+./cmake_build/ngen ./test/data/routing/gauge_01073000.gpkg '' ./test/data/routing/gauge_01073000.gpkg '' ./extern/summa/summa/test_ngen/provo_realization_config_w_summa_bmi.json
