a6fbd818acb59a64069914e113f10f6366003ee2
ashleymedin <ashley.vanbeusekom@gmail.com>
2025-12-15 19:31:20 +0900

    debug stuff

--- build/source/engine/soilLiqFlx.f90
+++ build/source/engine/soilLiqFlx.f90
@@ -1154,6 +1154,8 @@ contains
      dq_dHydStateVec(:) = realMissing ! not used, so cause problems
      dq_dNrgStateVec(:) = realMissing ! not used, so cause problems
    end if
+   print*,'DEBUG surfaceFlx infiltration derivatives: dq_dHydStateVec=',dq_dHydStateVec
+   print*,'DEBUG surfaceFlx infiltration derivatives: dq_dNrgStateVec=',dq_dNrgStateVec
   end associate
 
  end subroutine update_gather_runoff_components
