99ff6b430a0e83f30861a93d1e22279da05da966
ashleymedin <ashley.vanbeusekom@gmail.com>
2026-02-25 18:05:56 +0900

    just comment

--- build/source/dshare/popMetadat.f90
+++ build/source/dshare/popMetadat.f90
@@ -1047,7 +1047,7 @@ subroutine read_output_file(err,message)
           err=20; return
         endif
 
-      ! * grid variables do not change on less than annual-level
+      ! * grid variables do not change on less than annual-level, currently this will not output as type is 'unknown'
       case('grid')
         if(nWords<freqIndex) then
           freqName = 'empty'
@@ -1069,10 +1069,10 @@ subroutine read_output_file(err,message)
         endif
         if(trim(structName)=='time') then
           if (freqName/='timestep'.or. freqName/='1') then
-            write(*,*)'WARNING: time variable '//trim(varName)//': outputting variable at timestep level since it cannot be aggregated [entered "'//trim(freqName)//'"]'
+            write(*,*)'WARNING: time variable '//trim(varName)//': outputting time at timestep level since it cannot be aggregated [entered "'//trim(freqName)//'"]'
           endif
         else
-          write(*,*)'WARNING: temporally constant variable '//trim(varName)//': outputting variable in timestep file with no time dimension'
+          write(*,*)'WARNING: temporally constant variable '//trim(varName)//': outputting parameter in timestep file with no time dimension'
         endif
         iFreq = iLookFREQ%timestep
         freqName = 'timestep'
