8dfc4b97a80c044656e54515936d399c3ce870b9
ashleymedin <ashley.vanbeusekom@gmail.com>
2026-03-03 12:57:20 +0900

    space

--- build/source/engine/computResid.f90
+++ build/source/engine/computResid.f90
@@ -324,7 +324,7 @@ subroutine computResid(&
     if(any(isNan(rVec)))then
       write(*,'(a,1x,100(e12.5,1x))') 'rVec = ', rVec(min(iJac1,size(rVec)):min(iJac2,size(rVec)))
       write(*,'(a,1x,100(e12.5,1x))') 'fVec = ', fVec(min(iJac1,size(rVec)):min(iJac2,size(rVec)))
-       message=trim(message)//'NaN in residuals'; err=20; return
+      message=trim(message)//'NaN in residuals'; err=20; return
     endif
 
   end associate
