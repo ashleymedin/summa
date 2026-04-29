6a9389d3db8659b34b5de81a38e8289a5bfa883c
ashleymedin <ashley.vanbeusekom@gmail.com>
2026-04-29 17:34:53 +0900

    clear denormal also

--- build/source/driver/summa_modelRun.f90
+++ build/source/driver/summa_modelRun.f90
@@ -281,8 +281,9 @@ contains
                   elapsedUpdateArea,            & ! intent(inout): elapsed time for updating glacier and lake area for all GRUs (s)
                   err,cmessage)                   ! intent(out):   error control
 
-  ! Underflow occurs benignly and overflow occurs rarely in the physics; we do not want to stop the model when they occur
+  ! Underflow/denormal occur benignly and overflow occurs rarely in the physics; we do not want to stop the model when they occur
   call ieee_set_flag(ieee_underflow, .false.)
+  call ieee_set_flag(ieee_denormal, .false.)
   call ieee_set_flag(ieee_overflow, .false.)
 
   ! check errors
