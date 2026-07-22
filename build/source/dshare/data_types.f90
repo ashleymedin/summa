f8c48e3bf98e92242977ed9b5497a6ee4ab0f311
ashleymedin <ashley.vanbeusekom@gmail.com>
2026-07-22 02:35:37 -0500

    more sat soil

--- build/source/dshare/data_types.f90
+++ build/source/dshare/data_types.f90
@@ -620,6 +620,7 @@ MODULE data_types
  type, public :: in_type_surfaceFlux ! intent(in) data
    ! input: model control
    logical(lgt) :: firstSplitOper   ! flag indicating if desire to compute infiltration
+   integer(i4b) :: bc_lower         ! index defining the type of lower boundary conditions
    integer(i4b) :: ixInfRateMax     ! index defining the maximum infiltration rate method (GreenAmpt or topmodel_GA)
    integer(i4b) :: surfRun_SE       ! index defining the saturation excess surface runoff method
    integer(i4b) :: bc_upper         ! index defining the type of boundary conditions
@@ -1687,13 +1688,15 @@ contains
 
   associate(&
    ! model control
-   firstSplitOper         => in_soilLiqFlux % firstSplitOper,                      & ! flag to compute infiltration
+   firstSplitOper         => in_soilLiqFlux % firstSplitOper,                     & ! flag to compute infiltration
+   ixBcLowerSoilHydrology => model_decisions(iLookDECISIONS%bcLowrSoiH)%iDecision,& ! index of the lower boundary conditions for soil hydrology
    ixBcUpperSoilHydrology => model_decisions(iLookDECISIONS%bcUpprSoiH)%iDecision,& ! index defining the type of boundary conditions
    ixInfRateMax           => model_decisions(iLookDECISIONS%infRateMax)%iDecision,& ! index of the maximum infiltration rate parameterization
    surfRun_SE             => model_decisions(iLookDECISIONS%surfRun_SE)%iDecision & ! index defining the saturation excess surface runoff method
   &)
    ! intent(in): model control
    in_surfaceFlux % firstSplitOper = firstSplitOper          ! flag indicating if desire to compute infiltration
+   in_surfaceFlux % bc_lower       = ixBcLowerSoilHydrology  ! index defining the type of boundary conditions at the bottom of the soil
    in_surfaceFlux % bc_upper       = ixBcUpperSoilHydrology  ! index defining the type of boundary conditions (Neumann or Dirichlet)
    in_surfaceFlux % ixInfRateMax   = ixInfRateMax            ! index defining the maximum infiltration rate parameterization (GreenAmpt or topmodel_GA)
    in_surfaceFlux % surfRun_SE     = surfRun_SE              ! index defining the saturation excess surface runoff method
